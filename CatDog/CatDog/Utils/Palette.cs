0c91256e2b6d997138282d4165e26309b12e6eae
alexvitoriano <alex.vitoriano@gmail.com>
2018-09-29 00:27:21 -0300

    Palette and View Changes

---
+++ CatDog/CatDog/Utils/Palette.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace CatDog.Utils
+{
+    public class Palette
+    {
+        public static readonly Color DescTitle = Color.FromHex("A4CADC");
+
+        public static readonly Color CellColor = Color.FromHex("6388BF");
+
+        public static readonly Color Header = Color.FromHex("2F3EA1");
+    }
+}
---
+++ CatDog/CatDog/View/Breed/BreedListView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CatDog.View.Breed
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BreedListView : ContentPage
+	{
+		public BreedListView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
